d6ba1a5487b849078b8067f15d604855e1657c03
yod <yodgimel@yandex.com>
2016-05-15 16:00:55 -0700

    hot reloading enabled

--- server/server.js
+++ server/server.js
@@ -1,7 +1,19 @@
 var express = require('express');
 var path = require('path');
+//begin hot module loader with webpack
+var config = require('../webpack.config.js');
+var webpack = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
 
 var app = express();
+
+//makes webpack object
+var compiler = webpack(config);
+
+//when index.html calls for bundle.js it will serve hotmodule bundle.js using publicPath
+app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.publicPath}));
+app.use(webpackHotMiddleware(compiler));
 
 app.use(express.static('./dist'));
 
